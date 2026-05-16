72a4167d16cba5a01124d4d0faa007c223750466
pnaskardev <pnaskardev@gmail.com>
2026-05-16 17:44:47 +0530

    validation stage at the gateway

--- core/api/routes/routes.go
+++ core/api/routes/routes.go
@@ -2,7 +2,7 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v3"
-	auth_handler "github.com/pnaskardev/URL-Shortner-V1/core/pkg/handlers/auth"
+	authrepository "github.com/pnaskardev/URL-Shortner-V1/core/pkg/auth"
 )
 
 func ApiRouter(app *fiber.App) {
@@ -10,6 +10,5 @@ func ApiRouter(app *fiber.App) {
 	router := app.Group("/api")
 
 	authRouter := router.Group("/auth")
-	authRouter.Post("/sign-in", auth_handler.New().SignInHandler)
-
+	authRouter.Post("/sign-in", authrepository.New().SignInHandler)
 }
--- core/pkg/auth/authRepository.go
+++ core/pkg/auth/authRepository.go
@@ -1,4 +1,4 @@
-package auth_handler
+package authrepository
 
 import (
 	"github.com/gofiber/fiber/v3"
@@ -8,7 +8,10 @@ type Repository interface {
 	SignInHandler(c fiber.Ctx) error
 }
 
-type repository struct{}
+type repository struct {
+	// If we have DB client in here
+	// All of the routes will get the DB client and no need to make multiple connections
+}
 
 func New() Repository {
 	return &repository{}
