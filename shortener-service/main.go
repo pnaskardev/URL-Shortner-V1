a5e172f32e8edb6eb103fe5cbdd9c0c01bc43719
pnaskardev <pnaskardev@gmail.com>
2026-05-24 11:36:21 +0530

    added the shortener service posgres connection and config

--- shortener-service/main.go
+++ shortener-service/main.go
@@ -1,7 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/gofiber/fiber/v3/middleware/recover"
+	"github.com/google/uuid"
+	"github.com/pnaskardev/URL-Shortner-V1/shortener-service/config"
+	"github.com/pnaskardev/URL-Shortner-V1/shortener-service/infrastructure/database"
+)
 
 func main() {
-	fmt.Println("HELLO FROM URL SHORTENER SERVICE")
+	err := config.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	config := config.GetConfig()
+
+	_ = database.ConnectToPostgres()
+
+	customLogger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: false,
+		Level:     slog.LevelDebug,
+	}))
+	slog.SetDefault(customLogger)
+
+	app := fiber.New(
+		fiber.Config{
+			AppName: "SHORTENER SERVICE",
+		},
+	)
+	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use(func(c fiber.Ctx) error {
+
+		txID := c.Get("X-Transaction-ID")
+		if txID == "" {
+			txID = uuid.NewString()
+		}
+		c.Locals("transaction_id", txID)
+		c.Set("X-Transaction-ID", txID)
+
+		start := time.Now()
+		err := c.Next()
+		duration := time.Since(start)
+		c.Append("Server-Timing", "app;dur="+duration.String())
+		return err
+	})
+
+	// requestClient := &httpClients.RetryableHTTPClient{
+	// 	Client: &http.Client{
+	// 		Timeout: 5 * time.Second,
+	// 	},
+	// 	Config: httpClients.RetryConfig{
+	// 		MaxRetries: 3,
+	// 		BaseDelay:  100 * time.Millisecond,
+	// 		MaxDelay:   2 * time.Second,
+	// 	},
+	// }
+
+	// Register all of the routes
+	// routes.ApiRouter(app, requestClient, dbClient)
+
+	port := ":" + config.Port
+
+	go func() {
+		if err := app.Listen(port, fiber.ListenConfig{
+			EnablePrefork:     false,
+			EnablePrintRoutes: true,
+		}); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
+
+	_ = <-c // This blocks the main thread until an interrupt is received
+	fmt.Println("Gracefully shutting down...")
+	_ = app.Shutdown()
+
+	fmt.Println("Running cleanup tasks...")
+
+	// Your cleanup tasks go here
+	// db.Close()
+	// redisConn.Close()
+	fmt.Println("Fiber was successful shutdown.")
+
 }
